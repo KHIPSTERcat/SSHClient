29870c4da4442afb152d9c5b23f7a84f85555f85
KHIPSTERcat <cheshko.ilia@gmail.com>
2021-07-26 14:33:03 +1000

    Master

--- main.py
+++ main.py
@@ -6,23 +6,12 @@ username = 'chesh'
 secret = 'Ilia211000MICROSOFT'
 port = 22
 
-# client = paramiko.SSHClient()
-# client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-# # Подключение
-# client.connect(host, port, username, secret)
-#
-# ftp = client.open_sftp()
-# files = ftp.listdir()
-# print(files)
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# Подключение
+client.connect(host, port, username, secret)
 
-session = SshAuvLib.SshAuvSession(host, username, secret, port)
-
-session.connection()
-
-session.open_sftp_sessions()
-
-print(session.get_file_list())
-
-session.close()
-
-# client.close()
+ftp = client.open_sftp()
+files = ftp.listdir()
+print(files)
+client.close()
